365bb403bd2518278df6b539b3e04b6772ccbae2
ghaidaahaj42 <gaidaahaj2@gmail.com>
2024-11-04 23:03:06 +0200

    done

--- src/tools/footer.js
+++ src/tools/footer.js
@@ -1,37 +1,64 @@
 export default function Footer() {
   return (
-    <div className="container">
-      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-        <div className="col-md-4 d-flex align-items-center">
+    <section class="footercontainer">
+      <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div class="col-md-4 d-flex align-items-center logofooter">
           <a
             href="/"
-            className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1"
+            class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1"
           >
-            <h6>Spark</h6>
+            <img
+              class="bi"
+              width="30"
+              height="24"
+              src="./assets/2.png"
+              alt="logo"
+            />
           </a>
-          <span className="mb-3 mb-md-0 text-body-secondary">
+          <span class="mb-3 mb-md-0 text-body-secondary">
             © 2024 Company, Inc
           </span>
         </div>
 
-        <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
-          <li className="ms-3">
-            <a className="text-body-secondary" href="/">
-              <svg className="bi" width="24" height="24"></svg>
+        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+          <li class="ms-3">
+            <a
+              class="text-body-secondary"
+              href="https://drive.google.com/drive/u/0/home"
+            >
+              <img
+                class="bi"
+                width="30"
+                height="24"
+                src="./assets/instagram.png"
+                alt="insta"
+              />
             </a>
           </li>
-          <li className="ms-3">
-            <a className="text-body-secondary" href="/">
-              <svg className="bi" width="24" height="24"></svg>
+          <li class="ms-3">
+            <a class="text-body-secondary" href="#">
+              <img
+                class="bi"
+                width="30"
+                height="24"
+                src="./assets/whatsapp.png"
+                alt="whatsapp"
+              />
             </a>
           </li>
-          <li className="ms-3">
-            <a className="text-body-secondary" href="/">
-              <svg className="bi" width="24" height="24"></svg>
+          <li class="ms-3">
+            <a class="text-body-secondary" href="#">
+              <img
+                class="bi"
+                width="30"
+                height="24"
+                src="./assets/facebook.png"
+                alt="facebook"
+              />
             </a>
           </li>
         </ul>
       </footer>
-    </div>
+    </section>
   );
 }
